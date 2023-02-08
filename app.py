e53b6e5f81497f860ab65b6aa87403ce305b84f7
nijikara <jmffn317@gmail.com>
2023-02-08 13:10:57 +0900

    version change

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 from flask import request
 import datetime
 # import twitter_word_search_0209 as twitter_word_search
-import twitter_word_search_0208 as twitter_word_search
-# import twitter_word_search as twitter_word_search
+# import twitter_word_search_0208 as twitter_word_search
+import twitter_word_search as twitter_word_search
+
 from flask import Markup
 from dateutil.relativedelta import relativedelta
 
